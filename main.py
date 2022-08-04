1594dc264502f8c70be32235188b6a8e46d08a94
eltonsarmanho <s0@19t2&>
2022-08-04 16:37:11 -0300

    Done

--- main.py
+++ main.py
@@ -1,24 +1,49 @@
 import pandas as pd;
+from tkinter import filedialog
+import csv
+import chardet
 
 def load():
     # Use a breakpoint in the code line below to debug your script.
-    dataset = pd.read_csv('Dataset/Curso.csv',sep=',')
-    #dataset.drop(columns=['Carimbo de data/hora'],inplace=True)
+    filepath= filedialog.askopenfilename(initialdir="/home/eltonss/Downloads", title="Select file",
+                                               filetypes=(("CSV files", "*.csv"), ("all files", "*.*")))
+
+    charenc, delimiter = getInfoCSV(filepath)
+    dataset = pd.read_csv(filepath,sep=delimiter,encoding=charenc)
+
     print(dataset.head())
-
-    dataset['Nome'] = dataset['Nome'].str.upper()
-
-
     return dataset;
+
+def getInfoCSV(filepath):
+    delimiter = ''
+    charenc = ''
+    with open(filepath, encoding='latin-1') as csvfile:
+        dialect = csv.Sniffer().sniff(csvfile.read(1024))
+        rawdata = open(filepath, 'rb').read()
+        result = chardet.detect(rawdata)
+        charenc = result['encoding']
+        delimiter = dialect.delimiter
+
+    return charenc, delimiter
+
+def getColumn(index,dataset):
+    # calling the .columns
+    list_of_column_names = list(dataset.columns)
+    # displaying the list of column names
+    return list_of_column_names[index];
 
 def generaeLaTex(dataset):
 
-    for nome,local,curso,inicio,fim in zip(dataset['Nome'],dataset['Local'],dataset['Curso'],dataset['Inicio'],dataset['Fim']):
-        #string = "\para{\textbf{\color{azul} {0}}}{\color{azul}{1}}{part}{6}{}".format(nome,curso)
+
+    for nome,local,curso,inicio,fim in zip(dataset[getColumn(0,dataset)],
+                                           dataset[getColumn(4,dataset)],
+                                           dataset[getColumn(1,dataset)],
+                                           dataset[getColumn(5,dataset)],
+                                           dataset[getColumn(6,dataset)]):
         data = inicio[0:5]+' a '+fim[0:5]+' de 2022'
         cidade = local
         curso = curso
-        string = "\para{\\textbf{\color{azul}"+nome+"}}{\color{azul}"+curso+"}{part}{"+data+"}{"+cidade+"}{}"
+        string = "\para{\\textbf{\color{azul}"+nome.upper()+"}}{\color{azul}"+curso.upper()+"}{part}{"+data+"}{"+cidade+"}{}"
         print(string)
 
 # Press the green button in the gutter to run the script.
@@ -26,4 +51,3 @@ if __name__ == '__main__':
     dataset = load()
     generaeLaTex(dataset)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/

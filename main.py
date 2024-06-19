f42bbb76f02f76c84841701184a4ffb5f3009c88
eltonss <eltonss@ufpa.br>
2024-06-19 16:05:23 -0300

    Atualização do script

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pandas as pd;
 from tkinter import filedialog
 import csv
 import chardet
+from datetime import datetime
+import locale
 
 def load():
     # Use a breakpoint in the code line below to debug your script.
@@ -9,13 +11,29 @@ def load():
                                                filetypes=(("CSV files", "*.csv"), ("all files", "*.*")))
 
     charenc, delimiter = getInfoCSV(filepath)
-    print(charenc, delimiter)
     dataset = pd.read_csv(filepath,sep=',',encoding=charenc)
-    print(dataset.shape)
-    print(dataset.head())
+
     return dataset;
 
 
+def format_date_range(date_range):
+    # Configurar o locale para português do Brasil
+    locale.setlocale(locale.LC_TIME, 'pt_BR.UTF-8')
+    # Separar as partes da data
+    start_date, rest = date_range.split(' a ')
+    end_date, year = rest.split(' de ')
+
+    # Converter as partes das datas para objetos datetime
+    start_date_obj = datetime.strptime(start_date, "%d/%m")
+    end_date_obj = datetime.strptime(end_date, "%d/%m")
+
+    # Obter o mês em formato textual
+    month_name = start_date_obj.strftime("%B").capitalize()
+
+    # Construir a nova string formatada
+    formatted_date_range = f"{start_date_obj.day} a {end_date_obj.day} de {month_name} de {year}"
+
+    return formatted_date_range
 def getInfoCSV(filepath):
     # Detectar a codificação
     with open(filepath, 'rb') as rawdata:
@@ -55,9 +73,12 @@ def generaeLaTex(dataset):
                                            dataset[getColumn(4, dataset)],
                                            dataset[getColumn(5,dataset)]):
         data = inicio[0:5]+' a '+fim[0:5]+' de 2023'
+        ch = '9'
+        formatted_date_range = format_date_range(data)
+
         cidade = local
         curso = curso
-        string = "\para{\\textbf{\color{azul}"+nome.upper()+"}}{\color{azul}"+curso.upper()+"}{part}{"+data+"}{"+cidade+"}{}"
+        string = "\para{\\textbf{\color{azul}"+nome.upper()+"}}{\color{azul}"+curso.upper()+"}{part}{"+ch+"}{"+formatted_date_range+"}{"+cidade+"}{}"
         print(string)
 
 # Press the green button in the gutter to run the script.
